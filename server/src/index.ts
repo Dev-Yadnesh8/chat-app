f0be1186b3e0e842e8fa92eaeaa738b5b590c67f
Dev-Yadnesh8 <142681172+Dev-Yadnesh8@users.noreply.github.com>
2025-07-28 14:55:36 +0530

    added room based chat logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,22 +1,54 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { WebSocketServer ,WebSocket} from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: Number(process.env.PORT) });
 
 let userCount = 0;
-let allSockets:WebSocket[] = [];
+type websocketsType = Record<string, WebSocket[]>; // { 'room1code':[socket1,socket2]}
+let allSockets: websocketsType = {};
 
-wss.on("connection",(socket)=>{
-    allSockets.push(socket);
-    userCount++;
-    console.log("USER-CONNECTED-",userCount);
+interface ServerRes {
+  type: "chat";
+  payload: {
+    message: string;
+  };
+}
 
-    socket.on("message",(message)=>{
-        console.log("Incoming message ,",message.toString());
-        allSockets.forEach((s)=>s.send(message.toString()+"form server received"));
-    })
+wss.on("connection", (socket) => {
+  // allSockets.push(socket);
+  userCount++;
+  console.log("USER-CONNECTED-", userCount);
 
+  socket.on("message", (message) => {
+    const parsedMessage = JSON.parse(message.toString());
+    console.log(parsedMessage);
     
+    const { type, payload } = parsedMessage;
+
+    if (type === "join") {
+      const room = payload.roomCode;
+
+      if (!allSockets[room]) {
+        allSockets[room] = [];
+      }
+      allSockets[room].push(socket);
+      console.log(`Socket joined room ${room}`);
+      console.log(`Total sockets in room ${room}:`, allSockets[room].length);
+    } else if (type === "chat") {
+      const room = payload.roomCode;
+      const message = payload.message;
+      const sharedRoom = allSockets[room];
+      const response:ServerRes = {
+        type: "chat",
+        payload: {
+          message: message.toString(),
+        },
+      };
+      sharedRoom.forEach((e) => e.send(JSON.stringify(response)));
+    }
+  });
+
+  socket.on("close", (client) => {});
 });
